3859c3c2e82b03b71a6459bc2ab6acc7894bd3c5
Miguel-nextage <Pmiguelhkss@gmail.com>
2024-09-04 14:41:52 -0300

    Soma de dois inputs

---
+++ atividade01.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+N1 = int(input("digite o 1 valor"))
+N2 = int(input("digite o 2 valor"))
+
+soma = int(N1 + N2)
+
+print(F"seu valor é {soma}")
